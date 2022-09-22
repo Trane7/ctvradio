60501684176a5cc9d8e2ac23b8de73c18440aaa7
Dee <dgrowette@gmail.com>
2022-09-22 09:15:03 -0700

    took out some links in the footer

--- js/logic.js
+++ js/logic.js
@@ -68,17 +68,17 @@ function SlideShow(n) {
 var slidePosition = 0;
 SlideShow();
 
-function SlideShow() {
-  var i;
-  var slides = document.getElementsByClassName("Containers");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slidePosition++;
-  if (slidePosition > slides.length) {slidePosition = 0}
-  slides[slidePosition-1].style.display = "block";
-  setTimeout(SlideShow, 5000); // Change image every 2 seconds
-} 
+// function SlideShow() {
+//   var i;
+//   var slides = document.getElementsByClassName("Containers");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slidePosition++;
+//   if (slidePosition > slides.length) {slidePosition = 0}
+//   slides[slidePosition-1].style.display = "block";
+//   setTimeout(SlideShow, 5000); // Change image every 2 seconds
+// } 
 
 
 //Google Map API
